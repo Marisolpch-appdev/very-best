2f01ec2675c9f17391495549aad450f07eefe22d
Gerardo Ortiz <jortizdo@chicagobooth.edu>
2018-12-06 18:47:11 +0000

    Changes

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -16,6 +16,11 @@ class VenuesController < ApplicationController
   def show
     @bookmark = Bookmark.new
     @venue = Venue.find(params.fetch("id"))
+    
+    @url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@venue.address}&key=AIzaSyA5qwIlcKjijP_Ptmv46mk4cCjuWhSzS78"
+    @parsed_data = JSON.parse(open(@url).read)
+    @latitude = @parsed_data.dig("results", 0, "geometry", "location", "lat")
+    @longitude = @parsed_data.dig("results", 0, "geometry", "location", "lng")
 
     render("venues_templates/show.html.erb")
   end
